966f855a0ea88347eb49fbada2dbb1332ae4e149
Kurt Höblinger <kurt@Key-West.local>
2020-01-11 09:59:55 +0100

    initial release

---
+++ Eddi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eddi
+//
+//  Created by Kurt Höblinger on 11.01.20.
+//  Copyright © 2020 Kurt Höblinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
